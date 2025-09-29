115d368c6a25b98b57c3c400d1fc733e48a30f85
mohammadsarfarazafzal <mohammadsarfarazafzal@gmail.com>
2025-09-29 14:33:16 +0530

    Implemented User auth with JWT

---
+++ backend/src/main/java/com/studygroup/backend/service/UserService.java
@@ -0,0 +1,33 @@
+package com.studygroup.backend.service;
+
+import com.studygroup.backend.entity.User;
+import com.studygroup.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerUser(User user) {
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("Email already exists");
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+}
